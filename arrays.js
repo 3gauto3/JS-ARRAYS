166cce9237842c6b382aa9dff85f9f8511fc6f6a
Bryan Alejandro Gauto Gauto <bryanalejandrogautogauto@gmail.com>
2022-08-03 20:58:50 -0300

    proyecto final

--- arrays.js
+++ arrays.js
@@ -1,4 +1,16 @@
-const libros = ["los altisimos", "el cuaderno de las pesadillas", "coraline", "alguien en la ventana"]
+class libro {
+    constructor(titulo, autor, precio) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.precio = precio;
+    }
+}
+
+const libro1 = new libro ("el laberinto del fauno", "guillermo del toro", 4355);
+const libro2 = new libro ("los altisimos", "hugo correa", 3980);
+const libro3 = new libro ("la novia gitana", "caarmen corre", 4670);
+
+const libros = [libro1, libro2, libro3]
 
 let eliminarLibro = prompt("Ingresa el libro que deseas eliminar").toLowerCase();
 
